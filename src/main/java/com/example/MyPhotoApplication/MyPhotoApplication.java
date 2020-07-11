38fca53c9e662c2cb816957c495bc8bd7438a0db
Mohammad Raihan <mohammadrihan90@gmail.com>
2020-07-10 22:48:59 -0400

    First Commit

---
+++ src/main/java/com/example/MyPhotoApplication/MyPhotoApplication.java
@@ -0,0 +1,13 @@
+package com.example.MyPhotoApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPhotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPhotoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MyPhotoApplication/MyPhotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyPhotoApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPhotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
